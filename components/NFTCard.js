2eeed633576ea803e2faa4826fa9dbca9db4593a
david-aihe <doa9200@gmail.com>
2022-05-19 13:47:58 -0400

    rectangle button styled

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -25,21 +25,46 @@ const NFTCard = ({data}) => {
                 }}
             >
 
-            <Image
-                source={data.image}
-                resizeMode="cover"
-                style={{
-                    width: "100%",
-                    height: "100%",
-                    borderTopLeftRadius: SIZES.font,
-                    borderTopRightRadius: SIZES.font,
-                }}
-            />
+                <Image
+                    source={data.image}
+                    resizeMode="cover"
+                    style={{
+                        width: "100%",
+                        height: "100%",
+                        borderTopLeftRadius: SIZES.font,
+                        borderTopRightRadius: SIZES.font,
+                    }}
+                />
 
-            <CircleButton imgUrl={assets.heart} right={10} top={10} />
-         </View>
+                <CircleButton imgUrl={assets.heart} right={10} top={10} />
+            </View>
 
-         <SubInfo />
+            <SubInfo />
+
+            <View style={{ width: "100%", padding: SIZES.font }}>
+                <NFTTitle
+                    title={data.name}
+                    subTitle={data.creator}
+                    titleSize={SIZES.large}
+                    subTitleSize={SIZES.small}
+                />
+
+                <View
+                    style={{
+                        marginTop: SIZES.font,
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                    }}
+                >
+                    <EthPrice price={data.price} />
+                    <RectButton
+                        minWidth={120}
+                        fontSize={SIZES.font}
+                        handlePress={() => navigation.navigate("Details", { data })}
+                    />
+                </View>
+            </View>
 
         </View>
     )
